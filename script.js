868f94d4846fbf19ed8810ff8e0d24aad3c5d975
Jester <jester.sebastiansantos@gmail.com>
2026-04-27 13:10:07 -0500

    Setup functions

---
+++ script.js
@@ -0,0 +1,39 @@
+function add(num1, num2) {
+  return num1 + num2;
+}
+
+function subtract(num1, num2) {
+  return num1 - num2;
+}
+
+function multiply(num1, num2) {
+  return num1 * num2;
+}
+
+function divide(num1, num2) {
+  return num1 / num2;
+}
+
+function operate(operator, num1, num2) {
+  switch (operator) {
+    case "+":
+      add(num1, num2);
+      break;
+    case "-":
+      subtract(num1, num2);
+      break;
+    case "*":
+      multiply(num1, num2);
+      break;
+    case "/":
+      divide(num1, num2);
+      break;
+    default:
+      console.error("[operate] Invalid operator passed through");
+      break;
+  }
+}
+
+function main() {}
+
+main();
