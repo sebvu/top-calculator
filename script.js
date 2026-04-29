834c9dac6b9e855988d8c1fe4eb5a83c536dc3f8
Jester <jester.sebastiansantos@gmail.com>
2026-04-28 21:55:28 -0500

    Encapsulate functions in Calculator object

--- script.js
+++ script.js
@@ -1,3 +1,30 @@
+// number and operations are both referred to as ops in func names
+
+function Calculator() {
+  // public
+  this.operate = () => 1;
+  this.addOp = (op) => {
+    // verify is op is a number or an operator
+
+    // do not add if last element is not a number
+    if (isNaN(parseInt(currOperators.at(-1)))) return null;
+
+    currOperators.push(op);
+  };
+  this.popOp = () => {
+    if (currOperators.length === 0) return null; // return null if pop failed
+    currOperators.pop();
+  };
+  this.getOps = () => currOperators.join("");
+
+  // private
+  currOperators = []; // holds current nums and operators in its appropriate order
+  add = (num1, num2) => num1 + num2;
+  subtract = (num1, num2) => num1 - num2;
+  multiply = (num1, num2) => num1 * num2;
+  divide = (num1, num2) => num1 / num2;
+}
+
 function add(num1, num2) {
   return num1 + num2;
 }
@@ -35,36 +62,28 @@ function operate(operator, num1, num2) {
 }
 
 function main() {
-  // const vars
   const DEFAULT_DISPLAY_TEXT = "Insert";
 
-  // all operators
-  const addOp = document.getElementById("add-operator");
-  const subtractOp = document.getElementById("subtract-operator");
-  const multiplyOp = document.getElementById("multiply-operator");
-  const divideOp = document.getElementById("divide-operator");
-  const equalOp = document.getElementById("equal-operator");
-  const clearOp = document.getElementById("clear-operator");
-
-  // container elements
   const opsCtn = document.getElementById("calculator-operator-container");
   const calcDisplaySpan = document.querySelector(
     "#calculator-display-container span",
   );
 
-  ////
-
   calcDisplaySpan.textContent = DEFAULT_DISPLAY_TEXT;
-  let currOpsInserted = ""; // holder for current operations by user
+  let currOpsInserted = []; // holder for current operations by user
+
+  let calc = new Calculator();
 
   opsCtn.addEventListener("click", (e) => {
     let target = e.target; // target the id of button clicked
 
     switch (target.id) {
+      case "add-operator":
+        break;
     }
 
     // determine what text to fill display span with
-    if (currOpsInserted === "") {
+    if (currOpsInserted.join("") === "") {
       calcDisplaySpan.textContent = currOpsInserted;
     } else {
       calcDisplaySpan.textContent = DEFAULT_DISPLAY_TEXT;
